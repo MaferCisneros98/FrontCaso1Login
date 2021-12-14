b42a4c413c67768749a5566e85117016ee2738ca
Coris12 <corina.guzman.est@tecazuay.edu.ec>
2021-12-14 18:09:24 +0700

    arreglo de la clase informe rechazo
    informe rechazo

--- src/app/comercializadora/informe-rechazo/informe-rechazo.component.ts
+++ src/app/comercializadora/informe-rechazo/informe-rechazo.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/service/producto.service';
 
 @Component({
   selector: 'app-informe-rechazo',
@@ -8,9 +12,50 @@ import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 })
 export class InformeRechazoComponent implements OnInit {
 
-  constructor() { }
+  producto: Producto = null;
+
+  constructor(
+    private productoService: ProductoService,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router
+    ) { }
+
+
+  
   ngOnInit() {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.productoService.detail(id).subscribe(
+      data => {
+        this.producto = data;
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+        this.router.navigate(['/']);
+      }
+    );
   }
+  
+  onUpdate(): void {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.productoService.update(id, this.producto).subscribe(
+      data => {
+        this.toastr.success('Producto Actualizado', 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+        this.router.navigate(['/lista']);
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+        // this.router.navigate(['/']);
+      }
+    );
+  }
+ 
 
   displayBasic: boolean = false;
 
